532f56f3d6e84eec1ab2dba0cc78c27a6da6e280
KaiZhong <ZK14115@163.com>
2021-06-30 17:43:51 +0800

    BUGFIX: fix bugs in config

--- run_config.sh
+++ run_config.sh
@@ -3,6 +3,5 @@ mkdir ~/.tmux
 cp ./bash/bashrc ~/.bashrc
 cp ./vim/vimrc ~/.vim/vimrc
 cp -r ./tmux/plugins ~/.tmux/
-cp -r ./tmux/tmux-resurrect ~/.tmux/
 cp -r ./tmux/resurrect ~/.tmux/
 cp ./tmux/tmux.conf ~/.tmux.conf
