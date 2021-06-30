797fff8bfe0d105a3cb5dbe1da3b21360fbfa896
zhongkai <zhongkai@zktest.eva7.nics.cc>
2021-06-30 16:19:10 +0800

    FUNCADD: added run_config.sh script to copy bash, vim, tmux configs automatically

---
+++ run_config.sh
@@ -0,0 +1,8 @@
+mkdir ~/.vim
+mkdir ~/.tmux
+cp ./bash/bashrc ~/.bashrc
+cp ./vim/vimrc ~/.vim/vimrc
+cp -r ./tmux/plugins ~/.tmux/
+cp -r ./tmux/tmux-resurrect ~/.tmux/
+cp -r ./tmux/resurrect ~/.tmux/
+cp ./tmux/tmux.conf ~/.tmux.conf
