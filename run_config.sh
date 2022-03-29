037f78aaa83515d98620e643b88bb4046653da76
zhongkai <zhongkai@zktest.eva1.nics.info>
2022-03-29 14:28:54 +0000

    UPDATE bashrc

--- run_config.sh
+++ run_config.sh
@@ -1,8 +1,8 @@
 mkdir ~/.vim
 mkdir ~/.tmux
 echo mkdir vim tmux done
-#cp ./bash/bashrc ~/.bashrc
-#echo bashrc done
+cp ./bash/bashrc ~/.bashrc
+echo bashrc done
 cp ./vim/vimrc ~/.vim/vimrc
 echo vimrc done
 cp -r ./tmux/plugins ~/.tmux/
